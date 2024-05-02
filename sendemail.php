af8dab7b8f920ef4524fe1d8fb5498a7e70432dc
abyankamal <kamalmuhammad414@gmail.com>
2024-05-02 22:31:18 +0700

    sixteen commit

---
+++ sendemail.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    $to = "kamalmuhammad414@gmail.com";
+    $subject = "New Contact Form Submission";
+    $body = "Name: $name\nEmail: $email\nMessage: $message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Thank you for your message!";
+    } else {
+        echo "Something went wrong, please try again.";
+    }
+}
